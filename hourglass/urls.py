8d2a484dc82e8fed155ddd3a3d4dfc1cbf8b4d3c
Jesper Maassen <j.maassen93@gmail.com@users.noreply.github.com>
2020-12-03 23:33:22 +0100

    url pathing fix

--- hourglass/urls.py
+++ hourglass/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('timedcontracts.urls')),
-    path('play/', include('timedcontracts.urls')),
     path('admin/', admin.site.urls),
 ]
